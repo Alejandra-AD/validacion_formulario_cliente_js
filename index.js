888767ebb9f59ad7342c29cab1de190a8f51e8c2
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-09-17 20:27:01 -0300

    formulario fin cambios

--- index.js
+++ index.js
@@ -1,55 +1,66 @@
+const formulario = document.getElementById("formulario");
 
-const formulario = document.getElementById('formulario');
-const userName = document.getElementById('userName');
-const userEmail = document.getElementById('userEmail');
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+
+const alertSuccess = document.getElementById("alertSuccess");
+const alertName = document.getElementById("alertName");
+const alertEmail = document.getElementById("alertEmail");
+
 const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
 const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
-const alertName = document.getElementById('alertName');
-const alertEmail = document.getElementById('alertEmail');
-const alertSuccess = document.getElementById('alertSuccess');
-
-
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault(); //detiene lo que hace el navegador por defecto
-    console.log(userName.value);
-    //validando user name con js
-    if (!regUserName.test(userName.value) || !userName.value.trim()){
-        console.log("formato no válido name");
-        // pintarAlertaName();
-        return;
-    }
-
-    console.log(userEmail.value);
-
-    // devuelve true o false
-    if (!regUserEmail.test(userEmail.value)||!userEmail.value.trim()) {
-        console.log("formato no váilido email");
-        // pintarAlertaEmail();
-        return;
-    }
-    console.log(userEmail.value);
-
-    pintarMensajeExito();
-
-});
-
-
-// const pintarAlertaName = () => {
-
-//     alertName.classList.remove("d-none");
-
-// }
-// const pintarAlertaEmail = () => {
-
-
-//     alertEmail.classList.remove("d-none");
-
-
-// }
 
 const pintarMensajeExito = () => {
-
-
     alertSuccess.classList.remove("d-none");
+    alertSuccess.textContent = "Mensaje enviado con éxito";
+};
 
-}
+const pintarMensajeError = (errores) => {
+    errores.forEach((item) => {
+        item.tipo.classList.remove("d-none");
+        item.tipo.textContent = item.msg;
+    });
+};
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alertSuccess.classList.add("d-none");
+    const errores = [];
+
+    // validar nombre
+    if (!regUserName.test(userName.value) || !userName.value.trim()) {
+        userName.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertName,
+            msg: "Formato no válido campo nombre, solo letras",
+        });
+    } else {
+        userName.classList.remove("is-invalid");
+        userName.classList.add("is-valid");
+        alertName.classList.add("d-none");
+    }
+
+    // validar email
+    if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
+        userEmail.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertEmail,
+            msg: "Escriba un correo válido",
+        });
+    } else {
+        userEmail.classList.remove("is-invalid");
+        userEmail.classList.add("is-valid");
+        alertEmail.classList.add("d-none");
+    }
+
+    if (errores.length !== 0) {
+        pintarMensajeError(errores);
+        return;
+    }
+
+    console.log("Formulario enviado con éxito");
+    pintarMensajeExito();
+});
